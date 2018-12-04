c07df35060c8cd1f8568d0aa5c49a44f9ce94288
Алексей <MaskoLex@yandex.ru>
2018-12-04 16:51:37 +0300

    загрузка файлов

--- frontend/views/site/upload.php
+++ frontend/views/site/upload.php
@@ -6,13 +6,13 @@ use yii\helpers\FileHelper;
 ?>
 
 <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]) ?>
-<?= $form->field($model, 'img[]')->widget(FileInput::classname(), [
+<?= $form->field($model, 'img[]')->label(false)->widget(FileInput::classname(), [
     'options' => ['multiple' => true],
     'pluginOptions' => [
         'uploadUrl' => Url::to(['/site/upload']),
         'uploadExtraData' => [
-            'album_id' => 20,
-            'cat_id' => 'Nature'
+//            'album_id' => 20,
+ //           'cat_id' => 'Nature'
         ],
         'maxFileCount' => 10
     ]
